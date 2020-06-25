8c2b8755259987592a4922e977373087d3b5e705
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-06-25 13:59:35 +0300

    populate the sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def index
-
+    #@user=User.where(id: params[session[:id]])
   end
 
   def create
-    @user = User.where(name: params[:name])
+    @user = User.where(id:1)
     session[:id] = @user.id 
+    redirect_to index
   end
 end
