8e1048112ccdb4318ead3c917e10cbeaeba58d28
Christopher Amanor <kriskit7@gmail.com>
2020-06-26 13:26:19 +0000

    Add login and logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,5 +17,22 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:id] = nil
+    redirect_to sessions_path
+  end
+
+  def user_logged_in?
+    !session[:id].nil?
+  end
+
+   # Confirms a logged-in user.
+   def logged_in_user
+      unless logged_in?
+         flash[:danger] = "Please log in."
+         redirect_to sessions_path
+      end
+   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events, only: [:index, :show, :create, :new]
   resources :users, only: [:new, :create, :show]
-  resources :sessions, only: [:index, :create]
+  resources :sessions, only: [:index, :create, :destroy]
   root to: "sessions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
