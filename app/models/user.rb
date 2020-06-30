4629dfa3d8809af4ddfd7e09030909d5d2a19fb8
Christopher Amanor <kriskit7@gmail.com>
2020-06-30 10:14:38 +0000

    Add shoulda_matchers gem and added validations and tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :event_attendees, foreign_key: :attendee_id
   has_many :attended_events, through: :event_attendees, source: :attended_event
 
-    
+  validates :name, :email, presence: true
+  validates :email, length: { minimum: 12 }
+  validates :name, length: { minimum: 3 }
+
+
   def previous_events
     events.select{ |event| event  if event.date!=nil && event.date<= Date.today}
   end
