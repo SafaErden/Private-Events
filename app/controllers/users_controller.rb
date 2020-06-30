6d7ce014ddc8926f485a55c03c4c5876caf6cb8d
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-06-30 15:53:57 +0300

    add feature capybara

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :set_current_user, only: [:show]
-  before_action :authenticate_user
+  before_action :authenticate_user, except: [:new]
 
   # GET /users
   # GET /users.json
